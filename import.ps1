fad165fd0e2b7accef7d789d6e594aa1f8b5d9a8
Spencer Yoder <sryoder8@yahoo.com>
2022-12-16 14:08:28 -0600

    Update import.ps1

--- import.ps1
+++ import.ps1
@@ -1,20 +1,28 @@
-$excel = New-object -ComObject Excel.Application
+$excel = New-object -ComObject Excel.Application # Launch a new excel instance
 
-$workbook = $excel.workbooks.add()
-$sheet1 = $workbook.worksheets.item(1)
+$workbook = $excel.workbooks.add() # Create a new workbook
+$sheet1 = $workbook.worksheets.item(1) # Create a new sheet
 
-$lastrow = $workbook.worksheets.usedrange.rows.count + 1
+$lastrow = $workbook.worksheets.usedrange.rows.count + 1 # find the last row. Since this is a new worksheet, this will always be 1
 
-$excel.visible = $true
+$excel.visible = $true # Make the Excel instance visible
 
+# 1. List all the .csv files in the folder
+# 2. Parse out the full file name
+# 3. Inport the csv into powershell
+# 4. For ever file do the following
 Get-ChildItem -Filter *.csv | Select-Object -ExpandProperty FullName | Import-Csv | Foreach-Object { 
-    $sheet1.cells.item($lastrow, "a") = $_.a
-    $sheet1.cells.item($lastrow, "b") = $_.b
-    $sheet1.cells.item($lastrow, "c") = $_.c
-    $sheet1.cells.item($lastrow, "d") = $_.d
-    $sheet1.cells.item($lastrow, "e") = $_.e
-    
-    $lastrow++
+    $col = 0 # set a counter to tell what column is selected
+
+    # For every column on the row in the input csv, starting at "A"
+    foreach ($property in $_.PSObject.Property) {
+        $column = [char](65 + $col) # Convert column index into corresponding column letter. The powershell csv import saved them as a number.
+        $sheet1.cells.itme($lastrow, "$column") = $property.Value # Copy the contents of the cell and place it into the correct column at the bottom in the new excel doc
+        $col = $col + 1 # Move to the next column
+    }
+    $lastrow++ # Move to the next row
+
+    # Move to the next input file
 }
 
-Remove-Item *.csv
+Remove-Item *.csv # delete all the input .csv files
